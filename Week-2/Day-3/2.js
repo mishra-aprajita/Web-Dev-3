f90c5dc3e8b6a396858ab2a3c8d746a880c818cb
Aprajita <mishraaprajita43@gmail.com>
2026-08-13 10:00:52 +0530

    import,create,listen

---
+++ Week-2/Day-3/2.js
@@ -0,0 +1,14 @@
+//Step -1 import 
+
+const http = require("http");
+
+//step -2 create server 
+
+const server = http.createServer((request, response)=>{
+    response.end("welcome to my server");
+});
+
+//step -3 listen
+
+server.listen(8080);
+    
