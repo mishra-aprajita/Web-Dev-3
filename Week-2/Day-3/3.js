27247f956370aa125f1472eae49d19df023755a4
Aprajita <mishraaprajita43@gmail.com>
2026-08-17 00:47:20 +0530

    web browser

--- Week-2/Day-3/3.js
+++ Week-2/Day-3/3.js
@@ -1,17 +1,30 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+
     if (req.url == "/") {
         res.end("Home Page");
-    } else if (req.url == "/about") {
+
+    } else if (req.url == "/about" && req.method=="POST") {
         res.end("About Page");
-    } else if (req.url == "/data") {
-        res.end("Data Page");
+
+    } else if (req.url == "/data" && req.method=="GET") {
+        const data = fs.readFileSync("1.json");
+        res.end(data);
+
     } else {
         res.end("Not Found");
     }
+
 });
 
 server.listen(8080, () => {
     console.log("Server started");
 });
+
+
+/* Web browser only use get method to send request to server.that's why we use 
+ postmen to send rewuest to server by other methods like post,put,patch,delete etc.
+ and to test the api's. we can also use postman to send get request to server.*/
+
