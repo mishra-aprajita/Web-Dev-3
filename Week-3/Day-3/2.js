6e9b83723e925a476fc48ace3b9c766aa10b2c28
Aprajita <mishraaprajita43@gmail.com>
2026-08-20 22:35:16 +0530

    Nodemon

--- Week-3/Day-3/2.js
+++ Week-3/Day-3/2.js
@@ -5,13 +5,14 @@
 //step-3 npm run server 
 
 
+// 2.js
 const express = require("express");
-const fs = ("fs")
+const fs = require("fs");
 
 const app = express();
 
 // Routes
-
+// basic Route
 app.get("/", (req, res) => {
   res.send({ msg: "welcome to krmu application" });
 });
@@ -24,16 +25,38 @@ app.get("/login", (req, res) => {
   res.send({ msg: "welcome to login page" });
 });
 
-//Read All data [GET Method]
-app.get ("/read",(req, res)=>{
-    const data = fs.readFileSync("/krmu.json","utf-8");
-//console.log (data , typeof , data)
- 
-})
-app.get("/data", (req, res) => {
-  res.send("Data fetch successfully");
+// Read All Data [ GET Method]
+app.get("/read", (req, res) => {
+  const data = fs.readFileSync("./krmu.json", "utf-8");
+  // console.log(data, typeof data);
+
+  const jsdata = JSON.parse(data);
+  console.log(jsdata, typeof jsdata);
+
+  res.send(jsdata);
 });
 
+// Read student Data [ GET Method]
+app.get("/stread", (req, res) => {
+  const data = fs.readFileSync("./krmu.json", "utf-8");
+  // console.log(data, typeof data);
+
+  const jsdata = JSON.parse(data);
+  console.log(jsdata, typeof jsdata);
+
+  res.send(jsdata.student);
+});
+
+// Read trainer Data [ GET Method]
+app.get("/trread", (req, res) => {
+  const data = fs.readFileSync("./krmu.json", "utf-8");
+  // console.log(data, typeof data);
+
+  const jsdata = JSON.parse(data);
+  console.log(jsdata, typeof jsdata);
+
+  res.send(jsdata.trainer);
+});
 app.listen(8080, () => {
   console.log("server started");
 });
