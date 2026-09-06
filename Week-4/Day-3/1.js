37abf2187a21fbdbc8d0e8abf6206700e7825500
Aprajita <mishraaprajita43@gmail.com>
2026-09-06 23:40:40 +0530

    middleware

--- Week-4/Day-3/1.js
+++ Week-4/Day-3/1.js
@@ -1,65 +1,74 @@
 const express = require("express");
 const fs = require("fs");
 
-
 const app = express();
 
-//create a middlewere
+// create a middlewere
 const middlewere1 = (req, res, next) => {
-    console.log(1);
-    next(); // to move to next middleware or route handler
-    console.log(2);
+  console.log(1);
+  next();
+  console.log(2);
 };
 
 const middlewere2 = (req, res, next) => {
-    console.log(3);
+  console.log(3);
+  next();
+  console.log(4);
+};
+
+// use
+// app.use(middlewere1)
+// app.use(middlewere2)
+
+// 1. TimeLoggerMiddlewere
+
+const timeLoggerMiddlewere = (req, res, next) => {
+  const startTime = Date.now();
+  next();
+  const endTime = Date.now();
+
+  console.log(`Time Taken bu Route is ${endTime - startTime}ms`);
+};
+
+// app.use(timeLoggerMiddlewere);
+
+// RouteLoggerMiddlewere
+
+const routeLoggerMiddlewere = (req, res, next) => {
+  const record = `Route name is: ${req.url} and Method is: ${req.method}\n`;
+
+  fs.appendFileSync("./1.txt", record);
+  next();
+};
+
+// app.use(routeLoggerMiddlewere);
+
+// Watchman middlewere
+
+const watchmanMiddlewere = (req, res, next) => {
+  if (req.url == "/admin") {
+    res.send("not found");
+  } else {
     next();
-    console.log(4);
-}
+  }
+};
 
-// app.use(middlewere1); // use the middleware
-// app.use(middlewere2); // use the second middleware
-
-//1. Timeloggermiddlewere
-const timeloggerMiddlewere = (req, res, next) => {
-    const startTime = Date.now();
-    next();
-    const endTime = Date.now();
-
-    console.log(`Time taken to process the request: ${endTime - startTime} ms`);
-}
-app.use(timeloggerMiddlewere); // use the timelogger middleware
-
+app.use(watchmanMiddlewere)
 app.get("/home", (req, res) => {
-    console.log("home page");
-    res.send({ msg: "Home Page" });
+  console.log("home Page");
+  res.send("home Page");
 });
 
 app.get("/about", (req, res) => {
-    console.log("about page");
-    res.send({ msg: "About Page" });
+  console.log("about Page");
+  res.send("about Page");
 });
 
 app.get("/data", (req, res) => {
-    const data = fs.readFileSync("1.json", "utf-8");
-    res.send(data);
+  const data = fs.readFileSync("./1.json", "utf-8");
+  res.send(data);
 });
 
-
 app.listen(8080, () => {
-    console.log("Server started");
-})
-
-
-
-//type od middlewere --3 types
-//1. core m/w/inbuilt m/w
-//ex. express.json() ,   Router()
-//2. custom/internal m/w
-//ex. timelogger m/w , routelogger m/w, watchman m/w
-//3. External/Third party m/w--npm i , import, use
-//ex. multer, CORS
-
-
-
-//2. custom/internal m/w
+  console.log("server started");
+});
